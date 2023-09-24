bc3e25ef184b21c9f953c6605986e83d782370d5
Jasurbek <jasurdev1604@gmail.com>
2023-09-24 05:04:43 +0500

    Redux Toolkit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 60px;
+  background-color: black;
+`;
+
+const Link = styled(NavLink)`
+  color: #fff;
+  font-size: 30px;
+  font-weight: 600;
+  text-decoration: none;
+  cursor: pointer;
+  &.active {
+    color: coral;
+  }
+`;
 
 export default function Navbar() {
   return (
     <React.Fragment>
-      <h1>Navbar</h1>
+      <Container>
+        <Link to={"/home"}>Home</Link>
+        <Link to={"/body"}>Body</Link>
+        <Link to={"/about"}>About</Link>
+      </Container>
+      <Outlet />
     </React.Fragment>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -6,13 +6,22 @@ import { store } from "./redux/store.js";
 import Home from "./components/Home";
 import Body from "./components/Body";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import About from "./components/About";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Home />
-      <Body />
-      <Navbar />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route element={<Navbar />}>
+            <Route path={"/"} element={<Navigate to={"/home"} />} />
+            <Route path={"/home"} element={<Home />} />
+            <Route path={"/body"} element={<Body />} />
+            <Route path={"/about"} element={<About />} />
+          </Route>
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
