cc5e694695de635692576acd8a28c9d2e20d08a5
Jasurbek <jasurdev1604@gmail.com>
2023-09-25 23:33:31 +0500

    Redux Toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 import Home from "./components/Home";
 import Body from "./components/Body";
 import Navbar from "./components/Navbar";
@@ -10,14 +12,16 @@ import About from "./components/About";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route element={<Navbar />}>
-          <Route path={"/"} element={<Navigate to={"/home"} />} />
-          <Route path={"/home"} element={<Home />} />
-          <Route path={"/body"} element={<Body />} />
-          <Route path={"/about"} element={<About />} />
-        </Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route element={<Navbar />}>
+            <Route path={"/"} element={<Navigate to={"/home"} />} />
+            <Route path={"/home"} element={<Home />} />
+            <Route path={"/body"} element={<Body />} />
+            <Route path={"/about"} element={<About />} />
+          </Route>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
