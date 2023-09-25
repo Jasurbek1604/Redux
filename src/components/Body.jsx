76bf0757c05dcf6ff23b9d8a86d30452b09d4d5d
Jasurbek <jasurdev1604@gmail.com>
2023-09-25 23:31:52 +0500

    Redux Toolkit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../redux/Counter";
 
 export default function Body() {
-  const counter = useSelector((store) => store.counter);
-
-  const dispatch = useDispatch();
-
   return (
     <React.Fragment>
       <h1>Body</h1>
-      <h2>Count:{counter.data}</h2>
-      <button onClick={() => dispatch(increment())}>plus</button>
-      <button onClick={() => dispatch(decrement())}>minus</button>
     </React.Fragment>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
 import Home from "./components/Home";
 import Body from "./components/Body";
 import Navbar from "./components/Navbar";
@@ -12,16 +10,14 @@ import About from "./components/About";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route element={<Navbar />}>
-            <Route path={"/"} element={<Navigate to={"/home"} />} />
-            <Route path={"/home"} element={<Home />} />
-            <Route path={"/body"} element={<Body />} />
-            <Route path={"/about"} element={<About />} />
-          </Route>
-        </Routes>
-      </Provider>
+      <Routes>
+        <Route element={<Navbar />}>
+          <Route path={"/"} element={<Navigate to={"/home"} />} />
+          <Route path={"/home"} element={<Home />} />
+          <Route path={"/body"} element={<Body />} />
+          <Route path={"/about"} element={<About />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
