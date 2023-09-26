4b7dcb2e38ddbdff56de8819f4d5fa214d8b2db2
Jasurbek <jasurdev1604@gmail.com>
2023-09-27 01:24:11 +0500

    Redux Toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import Home from "./components/Home";
-import Body from "./components/Body";
-import Navbar from "./components/Navbar";
+import { store } from "./lesson1/redux/store";
+import Home from "./lesson1/components/Home";
+import Body from "./lesson1/components/Body";
+import Navbar from "./lesson1/components/Navbar";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import About from "./components/About";
+import About from "./lesson1/components/About";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
