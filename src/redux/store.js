3639119899f8fa28a6e3d97730fb8b0ec676aad1
Jasurbek <jasurdev1604@gmail.com>
2023-09-26 13:43:46 +0500

    Redux Toolkit

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counter from "./Counter";
-
-export const store = configureStore({
-  reducer: {
-    counter,
-    student: "Student",
-  },
-});
